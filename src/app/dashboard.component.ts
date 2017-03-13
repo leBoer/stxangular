ff1623dd6e81a7365ac03cab0374552606756d23
Nick Nisi <nick@nisi.org>
2017-03-13 14:56:46 +0400

    Adds open/closed indication

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Attribute } from '@angular/core';
 
 import { Exchange } from './exchange';
 import { ExchangeService } from './exchange.service';
+import { MomentModule } from 'angular2-moment';
+import * as moment from 'moment-timezone';
 
 @Component({
   moduleId: module.id,
@@ -20,11 +22,38 @@ export class DashboardComponent implements OnInit {
       .then(exchanges => this.exchanges = exchanges);
 
     this.utcTime();
+    // this.nonUTCTime(this.exchanges[1].timezone);
+  }
+
+  nonUTCTime(timezone): any {
+    var local_time = moment().tz(timezone);
+    return local_time;
   }
 
   utcTime(): void {
     setInterval(() => {
       this.myDate = new Date();
+      var format = 'hh:mm:ss';
+      // console.log(this.exchanges[1].timezone); // for testing purposes
+      // this.nonUTCTime(this.exchanges[1].timezone);
+      for (var i = 0; i < this.exchanges.length; i++) {
+        this.exchanges[i].time = this.nonUTCTime(this.exchanges[i].timezone).format("ddd HH:mm:ss");
+        this.exchanges[i].day = this.nonUTCTime(this.exchanges[i].timezone).format("dddd");
+        var time = moment(this.exchanges[i].time, format),
+          beforeTime = moment(this.exchanges[i].opening_time, format),
+          afterTime = moment(this.exchanges[i].closing_time, format);
+        if (time.isBetween(beforeTime, afterTime)) {
+          this.exchanges[i].open_status = true;
+        } else {
+          this.exchanges[i].open_status = false;
+        }
+        // if (this.exchanges[i].closing_time > this.nonUTCTime(this.exchanges[i].timezone) > this.exchanges[i].opening_time) {
+        //   console.log('Open!!');
+        // } else {
+        //   console.log('Closed!!');
+        // }
+      }
     }, 1000);
   }
+
 }
--- src/app/exchange.ts
+++ src/app/exchange.ts
@@ -2,4 +2,10 @@ export class Exchange {
   id: number;
   name: string;
   ticker: string;
+  timezone: string;
+  time: string;
+  day: string;
+  opening_time: number;
+  closing_time: number;
+  open_status: boolean;
 }
