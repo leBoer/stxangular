d40495f49b9e81c81cb5a68f6ae3993a0fc4019c
Nick Nisi <nick@nisi.org>
2017-03-15 14:16:22 +0400

    Adds local time for exchanges

--- src/app/clock.service.ts
+++ src/app/clock.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { MomentModule } from 'angular2-moment';
+import * as moment from 'moment-timezone';
 
 import { Exchange } from './exchange';
 
@@ -14,9 +16,17 @@ export class ClockService {
         return this.myDate = new Date;
     }
 
+    nonUTCTime(timezone): any {
+        var local_time = moment().tz(timezone);
+        return local_time;
+    }
+
     exchangeTimes(exchanges): any {
         for (var i = 0; i < exchanges.length; i++) {
             console.log('exchanges are running');
+            var exchange = exchanges[i];
+            exchange.time = this.nonUTCTime(exchange.timezone).format("ddd HH:mm:ss");
+            exchange.day = this.nonUTCTime(exchange.timezone).format("dddd");
         }
     }
 }
