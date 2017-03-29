1ba7a56542ee5e0f7660dbec07d9f68663bf6001
Nick Nisi <nick@nisi.org>
2017-03-29 17:44:40 +0400

    Fixes styling of heading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,21 @@ import { Observable } from 'rxjs/Rx';
   selector: 'stx-clock',
   styleUrls: [ './app.component.css' ],
   template: `
-    <div class="row">
-      <div class="col-12 title-div text-left">
-        <i class="fa fa-globe fa-2x" aria-hidden="true"></i>
-        <h1>    {{ title }}</h1>
+    <div class="container-fluid">
+      <div class="row background-row">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 title-div text-left">
+              <i class="fa fa-globe fa-2x" aria-hidden="true"></i>
+              <h1>    {{ title }}</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <app-tabset></app-tabset>
     <router-outlet></router-outlet>
   `
-    // <h1>{{title}}</h1>
-    // <nav>
-    //   <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-    //   <a routerLink="/exchanges" routerLinkActive="active">Exchanges</a>
-    // </nav>
-    // <router-outlet></router-outlet>
 })
 
 export class AppComponent {
