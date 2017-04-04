64436cf0694ce6d77466156d45a3fd9324086b2c
Nick Nisi <nick@nisi.org>
2017-04-04 14:17:10 +0400

    Working on styling the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,8 @@ import { Observable } from 'rxjs/Rx';
 @Component ({
   moduleId: module.id,
   selector: 'stx-clock',
+  templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ],
-  template: `
-    <div class="container-fluid">
-      <div class="row background-row">
-        <div class="container">
-          <div class="row">
-            <div class="col-12 title-div text-left">
-              <i class="fa fa-globe fa-2x" aria-hidden="true"></i>
-              <h1>    {{ title }}</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <app-tabset></app-tabset>
-    <router-outlet></router-outlet>
-  `
 })
 
 export class AppComponent {
